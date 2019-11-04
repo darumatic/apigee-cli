7409c123689425caf85aacb0af90d3caa8da1433
Matthew Delotavo <matthew.t.delotavo@gmail.com>
2019-11-04 11:17:44 +1100

    Update description

--- apigee/__init__.py
+++ apigee/__init__.py
@@ -1,7 +1,7 @@
 APP = 'apigeecli'
 CMD = 'apigee'
 __version__ = '0.7.1'
-description = 'Apigee Management API command-line interface easy-to-use OAuth2 authentication'
+description = 'Apigee Management API command-line interface with easy-to-use OAuth2 authentication'
 
 import json
 from os import getenv
