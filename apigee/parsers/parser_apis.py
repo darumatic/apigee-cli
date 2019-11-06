0cf52e309ae33bc1e1c73270afed1edc8a296ea3
Matthew Delotavo <matthew.t.delotavo@gmail.com>
2019-11-06 16:02:37 +1100

    Fix class ParserApis

--- apigee/parsers/parser_apis.py
+++ apigee/parsers/parser_apis.py
@@ -6,7 +6,8 @@ from apigee.api import deploy
 class ParserApis:
 
     def __init__(self, parser, **kwargs):
-        self._parser = parser.add_parser('apis', help='apis').add_subparsers()
+        self._parser = parser
+        self._parser_apis = self._parser.add_parser('apis', help='apis').add_subparsers()
         self._parent_parser = kwargs.get('parent_parser')
         self._dir_parser = kwargs.get('dir_parser')
         self._environment_parser = kwargs.get('environment_parser')
@@ -20,6 +21,14 @@ class ParserApis:
     @parser.setter
     def parser(self, value):
         self._parser = value
+
+    @property
+    def parser_apis(self):
+        return self._parser_apis
+
+    @parser_apis.setter
+    def parser_apis(self, value):
+        self._parser_apis = value
 
     @property
     def parent_parser(self):
@@ -57,7 +66,7 @@ class ParserApis:
         return self._parser
 
     def _build_apis_deploy_argument(self):
-        apis_deploy = self._parser.add_parser('deploy', help='deploy apis', parents=[self._parent_parser(), self._dir_parser(), self._environment_parser()])
+        apis_deploy = self._parser_apis.add_parser('deploy', help='deploy apis', parents=[self._parent_parser(), self._dir_parser(), self._environment_parser()])
         apis_deploy.add_argument('-n', '--name', help='name', required=True)
         # apis_deploy.add_argument('-d', '--directory', help='directory name')
         # apis_deploy.add_argument('-p', '--path', help='base path')
@@ -66,7 +75,7 @@ class ParserApis:
         apis_deploy.set_defaults(func=deploy.deploy)
 
     def _build_export_api_proxy_argument(self):
-        export_api_proxy = self._parser.add_parser('export', aliases=['export-api-proxy'], parents=[self._parent_parser()],
+        export_api_proxy = self._parser_apis.add_parser('export', aliases=['export-api-proxy'], parents=[self._parent_parser()],
             help='Outputs an API proxy revision as a ZIP formatted bundle of code and config files. This enables local configuration and development, including attachment of policies and scripts.')
         export_api_proxy.add_argument('-n', '--name', help='name', required=True)
         export_api_proxy.add_argument('-r', '--revision-number', help='revision number', required=True)
@@ -75,13 +84,13 @@ class ParserApis:
         export_api_proxy.set_defaults(func=apis.export_api_proxy)
 
     def _build_get_api_proxy_argument(self):
-        get_api_proxy = self._parser.add_parser('get', aliases=['get-api-proxy'], parents=[self._parent_parser()],
+        get_api_proxy = self._parser_apis.add_parser('get', aliases=['get-api-proxy'], parents=[self._parent_parser()],
             help='Gets an API proxy by name, including a list of existing revisions of the proxy.')
         get_api_proxy.add_argument('-n', '--name', help='name', required=True)
         get_api_proxy.set_defaults(func=lambda args: print(apis.get_api_proxy(args).text))
 
     def _build_list_api_proxies_argument(self):
-        list_api_proxies = self._parser.add_parser('list', aliases=['list-api-proxies'], parents=[self._parent_parser(), self._prefix_parser()],
+        list_api_proxies = self._parser_apis.add_parser('list', aliases=['list-api-proxies'], parents=[self._parent_parser(), self._prefix_parser()],
             help='Gets the names of all API proxies in an organization. The names correspond to the names defined in the configuration files for each API proxy.')
         list_api_proxies.set_defaults(func=lambda args: print(apis.list_api_proxies(args)))
 
