994a30f601672503b7a5fa1af8365ef489cb979e
Matthew Delotavo <matthew.t.delotavo@gmail.com>
2019-11-17 19:38:03 +1100

    Refactor Get API Proxy Deployment Details

--- apigee/api/deployments.py
+++ apigee/api/deployments.py
@@ -11,21 +11,6 @@ from apigee import APIGEE_ADMIN_API_URL
 from apigee.util import authorization
 from apigee.wrappers.deployments import get_api_proxy_deployment_details
 
-def get_api_proxy_deployment_details_formatted(args):
-    uri = '{}/v1/organizations/{}/apis/{}/deployments'.format(
-        APIGEE_ADMIN_API_URL, args.org, args.name)
-    hdrs = authorization.set_header({'Accept': 'application/json'}, args)
-    resp = requests.get(uri, headers=hdrs)
-    resp.raise_for_status()
-    # print(resp.status_code)
-    if args.revision_name:
-        revisions = [{'name':i['name'],'revision':[j['name'] for j in i['revision']]} for i in resp.json()['environment']]
-        if args.json:
-            return json.dumps(revisions)
-        pd.set_option('display.max_colwidth', args.max_colwidth)
-        return pd.DataFrame.from_dict(json_normalize(revisions), orient='columns')
-    return resp.text
-
 @get_api_proxy_deployment_details
 def get_api_proxy_deployment_details(args):
     uri = '{}/v1/organizations/{}/apis/{}/deployments'.format(
@@ -35,3 +20,19 @@ def get_api_proxy_deployment_details(args):
     resp.raise_for_status()
     # print(resp.status_code)
     return resp
+
+def get_api_proxy_deployment_details_formatted(args):
+    resp = get_api_proxy_deployment_details(args)
+    if args.revision_name:
+        revisions = []
+        for i in resp.json()['environment']:
+            revisions.append({
+                'name':i['name'],'revision':[
+                    j['name'] for j in i['revision']
+                ]
+            })
+        if args.json:
+            return json.dumps(revisions)
+        pd.set_option('display.max_colwidth', args.max_colwidth)
+        return pd.DataFrame.from_dict(json_normalize(revisions), orient='columns')
+    return resp.text
