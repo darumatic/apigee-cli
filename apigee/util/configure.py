090d62c897c8d3f11e946e684bba76cfbf441d0a
Matthew Delotavo <matthew.t.delotavo@gmail.com>
2019-11-25 15:02:47 +1100

    Refactor class ParserConfigure

--- apigee/util/configure.py
+++ apigee/util/configure.py
@@ -10,34 +10,29 @@ from apigee.util.io import IO
 
 class Configure:
 
+    KEY_LIST = (
+        'username',
+        'password',
+        'mfa_secret',
+        'org',
+        'prefix'
+    )
+
     def __init__(self, args):
         self._args = args
         self._profile = args.profile
         self._config = configparser.ConfigParser()
         self._config.read(APIGEE_CLI_CREDENTIALS_FILE)
-        self._username = self._get_config(self._profile, 'username')
-        self._password = self._get_config(self._profile, 'password')
-        self._mfa_secret = self._get_config(self._profile, 'mfa_secret')
-        self._org = self._get_config(self._profile, 'org')
-        self._prefix = self._get_config(self._profile, 'prefix')
+        try:
+            self._profile_dict = dict(self._config._sections[self._profile])
+            for key in self.KEY_LIST:
+                if key not in self._profile_dict:
+                    self._profile_dict[key] = None
+        except KeyError:
+            self._profile_dict = {k: None for k in self.KEY_LIST}
 
     def __call__(self):
         self._main()
-
-    def _get_config(self, section, key):
-        try:
-            return self._config[section][key]
-        except:
-            return None
-
-    def _gen_profile_dict(self):
-        return {
-            'username': self._username,
-            'password': self._password,
-            'mfa_secret': self._mfa_secret,
-            'org': self._org,
-            'prefix': self._prefix
-        }
 
     def _remove_empty_keys(self, dict):
         return {k: v for k, v in dict.items() if v}
@@ -48,11 +43,11 @@ class Configure:
             self._config.write(configfile)
 
     def _main(self):
-        self._username   = input('Apigee username (email) [{}]: '                    .format(self._username))
-        self._password   = input('Apigee password [{}]: '                            .format(self._password))
-        self._mfa_secret = input('Apigee MFA key (recommended) [{}]: '               .format(self._mfa_secret))
-        self._org        = input('Default Apigee organization (recommended) [{}]: '  .format(self._org))
-        self._prefix     = input('Default team/resource prefix (recommended) [{}]: ' .format(self._prefix))
+        self._profile_dict['username']   = input('Apigee username (email) [{}]: '                    .format(self._profile_dict['username']))
+        self._profile_dict['password']   = input('Apigee password [{}]: '                            .format(self._profile_dict['password']))
+        self._profile_dict['mfa_secret'] = input('Apigee MFA key (recommended) [{}]: '               .format(self._profile_dict['mfa_secret']))
+        self._profile_dict['org']        = input('Default Apigee organization (recommended) [{}]: '  .format(self._profile_dict['org']))
+        self._profile_dict['prefix']     = input('Default team/resource prefix (recommended) [{}]: ' .format(self._profile_dict['prefix']))
 
-        self._config[self._profile] = self._remove_empty_keys(self._gen_profile_dict())
+        self._config[self._profile] = self._remove_empty_keys(self._profile_dict)
         self._save_config(APIGEE_CLI_CREDENTIALS_FILE)
