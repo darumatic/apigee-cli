c3a457e8a9a423016c0d21b5e46e5e35e042d3aa
Matthew Delotavo <matthew.t.delotavo@gmail.com>
2019-11-25 16:14:01 +1100

    Refactor Pull API proxy

--- apigee/abstract/pull/apis.py
+++ apigee/abstract/pull/apis.py
@@ -1,18 +1,11 @@
 #!/usr/bin/env python
 """https://apidocs.apigee.com/api-reference/content/api-proxies"""
 
-import json
 import os
-import requests
-import sys
-import xml.etree.ElementTree as et
-import zipfile
 from abc import ABC, abstractmethod
 from pathlib import Path
 
 from apigee.util.io import IO
-from apigee.api.keyvaluemaps import get_keyvaluemap_in_an_environment
-from apigee.api.targetservers import get_targetserver
 
 class IPull(IO):
 
@@ -90,108 +83,37 @@ class IPull(IO):
     def apiproxy_dir(self, value):
         self._apiproxy_dir = value
 
+    @abstractmethod
     def get_apiproxy_files(self, directory):
-        files = []
-        for filename in Path(directory+'/apiproxy/').resolve().rglob('*'):
-            files.append(str(filename))
-        return files
+        pass
 
+    @abstractmethod
     def get_keyvaluemap_dependencies(self, files):
-        kvms = []
-        for f in files:
-            try:
-                root = et.parse(f).getroot()
-                if root.tag == 'KeyValueMapOperations':
-                    kvms.append(root.attrib['mapIdentifier'])
-            except:
-                pass
-        return kvms
+        pass
 
+    @abstractmethod
     def export_keyvaluemap_dependencies(self, args, kvms, kvms_dir, force=False):
-        if not os.path.exists(kvms_dir):
-            os.makedirs(kvms_dir)
-        for kvm in kvms:
-            kvm_file = kvms_dir+'/'+kvm
-            if not force:
-                self.path_exists(kvm_file)
-            print('Pulling', kvm, 'and writing to', os.path.abspath(kvm_file))
-            args.name = kvm
-            resp = get_keyvaluemap_in_an_environment(args).text
-            print(resp)
-            with open(kvm_file, 'w') as f:
-                f.write(resp)
+        pass
 
+    @abstractmethod
     def get_targetserver_dependencies(self, files):
-        target_servers = []
-        for f in files:
-            try:
-                root = et.parse(f).getroot()
-                for child in root.iter('Server'):
-                    target_servers.append(child.attrib['name'])
-            except:
-                pass
-        return target_servers
+        pass
 
+    @abstractmethod
     def export_targetserver_dependencies(self, args, target_servers, target_servers_dir, force=False):
-        if not os.path.exists(target_servers_dir):
-            os.makedirs(target_servers_dir)
-        for ts in target_servers:
-            ts_file = target_servers_dir+'/'+ts
-            if not force:
-                self.path_exists(ts_file)
-            print('Pulling', ts, 'and writing to', os.path.abspath(ts_file))
-            args.name = ts
-            resp = get_targetserver(args).text
-            print(resp)
-            with open(ts_file, 'w') as f:
-                f.write(resp)
+        pass
 
+    @abstractmethod
     def prefix_dependencies_in_work_tree(self):
-        prefix = self._prefix
-        dependencies = [i for i in self._dependencies if not i.startswith(prefix)]
-        directory = self._work_tree
-        files = []
-        for filename in Path(directory).resolve().rglob('*'):
-            if not os.path.isdir(str(filename)) and '.git' not in str(filename):
-                files.append(str(filename))
-        print('Prefixing', dependencies, 'with', prefix)
-        for dep in dependencies:
-            for file in files:
-                with open(file, 'r') as f:
-                    body = None
-                    try:
-                        body = f.read()
-                    except Exception as e:
-                        print(type(e).__name__, e)
-                        print('Ignoring', file)
-                    if dep in body:
-                        with open(file, 'w') as new_f:
-                            new_f.write(body.replace(dep, prefix+dep))
-                        print('M  ', os.path.abspath(file))
+        pass
 
+    @abstractmethod
     def get_apiproxy_basepath(self, directory):
-        default_file = directory+'/apiproxy/proxies/default.xml'
-        tree = et.parse(default_file)
-        try:
-            return tree.find('.//BasePath').text, default_file
-        except AttributeError as ae:
-            sys.exit('No BasePath found in ' + default_file)
+        pass
 
+    @abstractmethod
     def set_apiproxy_basepath(self, basepath, file):
-        default_file = os.path.abspath(file)
-        tree = et.parse(default_file)
-        current_basepath = None
-        try:
-            current_basepath = tree.find('.//BasePath').text
-        except AttributeError as ae:
-            sys.exit('No BasePath found in ' + default_file)
-        with open(default_file, 'r+') as f:
-            body = f.read().replace(current_basepath, basepath)
-            f.seek(0)
-            f.write(body)
-            f.truncate()
-        print(current_basepath, '->', basepath)
-        print('M  ', default_file)
+        pass
 
     @abstractmethod
     def pull(self):
--- apigee/api/pull/apis.py
+++ apigee/api/pull/apis.py
@@ -11,11 +11,116 @@ from pathlib import Path
 
 from apigee.abstract.pull.apis import IPull
 from apigee.api.apis import export_api_proxy
+from apigee.api.keyvaluemaps import get_keyvaluemap_in_an_environment
+from apigee.api.targetservers import get_targetserver
 
 class Pull(IPull):
 
     def __init__(self, args):
         super().__init__(args)
+
+    def get_apiproxy_files(self, directory):
+        files = []
+        for filename in Path(directory+'/apiproxy/').resolve().rglob('*'):
+            files.append(str(filename))
+        return files
+
+    def get_keyvaluemap_dependencies(self, files):
+        kvms = []
+        for f in files:
+            try:
+                root = et.parse(f).getroot()
+                if root.tag == 'KeyValueMapOperations':
+                    kvms.append(root.attrib['mapIdentifier'])
+            except:
+                pass
+        return kvms
+
+    def export_keyvaluemap_dependencies(self, args, kvms, kvms_dir, force=False):
+        if not os.path.exists(kvms_dir):
+            os.makedirs(kvms_dir)
+        for kvm in kvms:
+            kvm_file = kvms_dir+'/'+kvm
+            if not force:
+                self.path_exists(kvm_file)
+            print('Pulling', kvm, 'and writing to', os.path.abspath(kvm_file))
+            args.name = kvm
+            resp = get_keyvaluemap_in_an_environment(args).text
+            print(resp)
+            with open(kvm_file, 'w') as f:
+                f.write(resp)
+
+    def get_targetserver_dependencies(self, files):
+        target_servers = []
+        for f in files:
+            try:
+                root = et.parse(f).getroot()
+                for child in root.iter('Server'):
+                    target_servers.append(child.attrib['name'])
+            except:
+                pass
+        return target_servers
+
+    def export_targetserver_dependencies(self, args, target_servers, target_servers_dir, force=False):
+        if not os.path.exists(target_servers_dir):
+            os.makedirs(target_servers_dir)
+        for ts in target_servers:
+            ts_file = target_servers_dir+'/'+ts
+            if not force:
+                self.path_exists(ts_file)
+            print('Pulling', ts, 'and writing to', os.path.abspath(ts_file))
+            args.name = ts
+            resp = get_targetserver(args).text
+            print(resp)
+            with open(ts_file, 'w') as f:
+                f.write(resp)
+
+    def prefix_dependencies_in_work_tree(self):
+        prefix = self._prefix
+        dependencies = [i for i in self._dependencies if not i.startswith(prefix)]
+        directory = self._work_tree
+        files = []
+        for filename in Path(directory).resolve().rglob('*'):
+            if not os.path.isdir(str(filename)) and '.git' not in str(filename):
+                files.append(str(filename))
+        print('Prefixing', dependencies, 'with', prefix)
+        for dep in dependencies:
+            for file in files:
+                with open(file, 'r') as f:
+                    body = None
+                    try:
+                        body = f.read()
+                    except Exception as e:
+                        print(type(e).__name__, e)
+                        print('Ignoring', file)
+                    if dep in body:
+                        with open(file, 'w') as new_f:
+                            new_f.write(body.replace(dep, prefix+dep))
+                        print('M  ', os.path.abspath(file))
+
+    def get_apiproxy_basepath(self, directory):
+        default_file = directory+'/apiproxy/proxies/default.xml'
+        tree = et.parse(default_file)
+        try:
+            return tree.find('.//BasePath').text, default_file
+        except AttributeError as ae:
+            sys.exit('No BasePath found in ' + default_file)
+
+    def set_apiproxy_basepath(self, basepath, file):
+        default_file = os.path.abspath(file)
+        tree = et.parse(default_file)
+        current_basepath = None
+        try:
+            current_basepath = tree.find('.//BasePath').text
+        except AttributeError as ae:
+            sys.exit('No BasePath found in ' + default_file)
+        with open(default_file, 'r+') as f:
+            body = f.read().replace(current_basepath, basepath)
+            f.seek(0)
+            f.write(body)
+            f.truncate()
+        print(current_basepath, '->', basepath)
+        print('M  ', default_file)
 
     def pull(self):
         args = self._args
