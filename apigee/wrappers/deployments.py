2160f3e6b0e453d4f6e7fc3fc18d5b56b82a3db5
Matthew Delotavo <matthew.t.delotavo@gmail.com>
2019-11-26 14:20:45 +1100

    deleted:    apigee/wrappers/

--- apigee/wrappers/deployments.py
+++
@@ -1,8 +0,0 @@
-import functools
-
-def get_api_proxy_deployment_details(func):
-    @functools.wraps(func)
-    def wrapper(fargs, *args, **kwargs):
-        result = func(fargs)
-        return result
-    return wrapper
