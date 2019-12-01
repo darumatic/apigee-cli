b025842afb7a5fa651daefff1a7ca5fa5535dfd6
Matthew Delotavo <matthew.t.delotavo@gmail.com>
2019-12-02 10:46:37 +1100

    Refactor pull, push

--- apigee/parsers/parser_apis.py
+++ apigee/parsers/parser_apis.py
@@ -1,8 +1,7 @@
 import argparse
 
 from apigee.api import deploy
-from apigee.api.apis import Apis
-from apigee.pull.apis import Pull
+from apigee.api.apis import Apis, Pull
 
 from apigee.parsers.parent_parser import ParentParser
 from apigee.parsers.dir_parser import DirParser
