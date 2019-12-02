53bc21d0646bb2c93b48cb002fb91f79aa352a6b
Matthew Delotavo <matthew.t.delotavo@gmail.com>
2019-12-02 13:55:55 +1100

    Refactor Delete undeployed API Proxy Revisions

--- apigee/api/apis.py
+++ apigee/api/apis.py
@@ -40,28 +40,29 @@ class Apis(IApis):
         return resp
 
     def delete_undeployed_revisions(self, save_last=0, dry_run=False):
-        revisions = self.list_api_proxy_revisions().json()
-        deployment_details = []
-        for i in Deployments(self._auth, self._org_name, self._api_name).get_api_proxy_deployment_details().json()['environment']:
-            deployment_details.append({
-                'name':i['name'],'revision':[
-                    j['name'] for j in i['revision']
+        def gen_deployment_detail(deployment):
+            return {
+                'name':deployment['name'],'revision':[
+                    revision['name'] for revision in deployment['revision']
                 ]
-            })
+            }
+        def delete_revisions(revision_number):
+            print('Deleting revison', revision_number)
+            self.delete_api_proxy_revision(revision_number)
+        revisions = self.list_api_proxy_revisions().json()
+        deployments = Deployments(self._auth, self._org_name, self._api_name).get_api_proxy_deployment_details().json()['environment']
+        deployment_details = list(map(gen_deployment_detail, deployments))
         deployed = []
         for dep in deployment_details:
             deployed.extend(dep['revision'])
         deployed = list(set(deployed))
         undeployed = [rev for rev in revisions if rev not in deployed]
-        undeployed = [int(x) for x in undeployed]
+        undeployed = [int(rev) for rev in undeployed]
         undeployed.sort()
         undeployed = undeployed[:len(undeployed)-save_last]
         print('Undeployed revisions:', undeployed)
         if not dry_run:
-            for rev in undeployed:
-                revision_number = rev
-                print('Deleting revison', rev)
-                self.delete_api_proxy_revision(revision_number)
+            list(map(delete_revisions, undeployed))
 
     def export_api_proxy(self, revision_number, writezip=True, output_file=None):
         uri = '{0}/v1/organizations/{1}/apis/{2}/revisions/{3}?format=bundle'.format(APIGEE_ADMIN_API_URL, self._org_name, self._api_name, revision_number)
