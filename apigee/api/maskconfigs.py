8bef052ee73e4b2b5dc413714d87511b02015bf4
Matthew Delotavo <matthew.t.delotavo@gmail.com>
2019-12-04 15:37:07 +1100

    Create classes Maskconfigs, IMaskconfigs, MaskconfigsSerializer

--- apigee/api/maskconfigs.py
+++ apigee/api/maskconfigs.py
@@ -1,54 +1,55 @@
 #!/usr/bin/env python
 """https://apidocs.apigee.com/api-reference/content/data-masks"""
 
-import requests
 import json
+import requests
 
 from apigee import APIGEE_ADMIN_API_URL
+from apigee.abstract.api.maskconfigs import IMaskconfigs
 from apigee.util import authorization
 
-def create_data_masks_for_an_api_proxy(args):
-    uri = '{}/v1/organizations/{}/apis/{}/maskconfigs'.format(
-        APIGEE_ADMIN_API_URL, args.org, args.name)
-    hdrs = authorization.set_header({'Accept': 'application/json', 'Content-Type': 'application/json'}, args)
-    body = json.loads(args.body)
-    resp = requests.post(uri, headers=hdrs, json=body)
-    resp.raise_for_status()
-    # print(resp.status_code)
-    return resp
+class Maskconfigs(IMaskconfigs):
 
-def delete_data_masks_for_an_api_proxy(args):
-    uri = '{}/v1/organizations/{}/apis/{}/maskconfigs/{}'.format(
-        APIGEE_ADMIN_API_URL, args.org, args.name, args.maskconfig_name)
-    hdrs = authorization.set_header({'Accept': 'application/json'}, args)
-    resp = requests.delete(uri, headers=hdrs)
-    resp.raise_for_status()
-    # print(resp.status_code)
-    return resp
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-def get_data_mask_details_for_an_api_proxy(args):
-    uri = '{}/v1/organizations/{}/apis/{}/maskconfigs/{}'.format(
-        APIGEE_ADMIN_API_URL, args.org, args.name, args.maskconfig_name)
-    hdrs = authorization.set_header({'Accept': 'application/json'}, args)
-    resp = requests.get(uri, headers=hdrs)
-    resp.raise_for_status()
-    # print(resp.status_code)
-    return resp
+    def create_data_masks_for_an_api_proxy(self, request_body):
+        uri = '{0}/v1/organizations/{1}/apis/{2}/maskconfigs'.format(APIGEE_ADMIN_API_URL, self._org_name, self._api_name)
+        hdrs = authorization.set_header({'Accept': 'application/json', 'Content-Type': 'application/json'}, self._auth)
+        body = json.loads(request_body)
+        resp = requests.post(uri, headers=hdrs, json=body)
+        resp.raise_for_status()
+        # print(resp.status_code)
+        return resp
 
-def list_data_masks_for_an_api_proxy(args):
-    uri = '{}/v1/organizations/{}/apis/{}/maskconfigs'.format(
-        APIGEE_ADMIN_API_URL, args.org, args.name)
-    hdrs = authorization.set_header({'Accept': 'application/json'}, args)
-    resp = requests.get(uri, headers=hdrs)
-    resp.raise_for_status()
-    # print(resp.status_code)
-    return resp
+    def delete_data_masks_for_an_api_proxy(self, maskconfig_name):
+        uri = '{0}/v1/organizations/{1}/apis/{2}/maskconfigs/{3}'.format(APIGEE_ADMIN_API_URL, self._org_name, self._api_name, maskconfig_name)
+        hdrs = authorization.set_header({'Accept': 'application/json'}, self._auth)
+        resp = requests.delete(uri, headers=hdrs)
+        resp.raise_for_status()
+        # print(resp.status_code)
+        return resp
 
-def list_data_masks_for_an_organization(args):
-    uri = '{}/v1/organizations/{}/maskconfigs'.format(
-        APIGEE_ADMIN_API_URL, args.org)
-    hdrs = authorization.set_header({'Accept': 'application/json'}, args)
-    resp = requests.get(uri, headers=hdrs)
-    resp.raise_for_status()
-    # print(resp.status_code)
-    return resp
+    def get_data_mask_details_for_an_api_proxy(self, maskconfig_name):
+        uri = '{0}/v1/organizations/{1}/apis/{2}/maskconfigs/{3}'.format(APIGEE_ADMIN_API_URL, self._org_name, self._api_name, maskconfig_name)
+        hdrs = authorization.set_header({'Accept': 'application/json'}, self._auth)
+        resp = requests.get(uri, headers=hdrs)
+        resp.raise_for_status()
+        # print(resp.status_code)
+        return resp
+
+    def list_data_masks_for_an_api_proxy(self):
+        uri = '{0}/v1/organizations/{1}/apis/{2}/maskconfigs'.format(APIGEE_ADMIN_API_URL, self._org_name, self._api_name)
+        hdrs = authorization.set_header({'Accept': 'application/json'}, self._auth)
+        resp = requests.get(uri, headers=hdrs)
+        resp.raise_for_status()
+        # print(resp.status_code)
+        return resp
+
+    def list_data_masks_for_an_organization(self):
+        uri = '{0}/v1/organizations/{1}/maskconfigs'.format(APIGEE_ADMIN_API_URL, self._org_name)
+        hdrs = authorization.set_header({'Accept': 'application/json'}, self._auth)
+        resp = requests.get(uri, headers=hdrs)
+        resp.raise_for_status()
+        # print(resp.status_code)
+        return resp
