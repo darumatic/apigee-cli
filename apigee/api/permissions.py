8b2b8dcc059874f8ef800889ffd498529d3c14c3
Matthew Delotavo <matthew.t.delotavo@gmail.com>
2019-12-06 15:21:01 +1100

    Update default Permissions for a team role

--- apigee/api/permissions.py
+++ apigee/api/permissions.py
@@ -26,8 +26,98 @@ class Permissions(IPermissions):
         uri = '{0}/v1/organizations/{1}/userroles/{2}/resourcepermissions'.format(APIGEE_ADMIN_API_URL, self._org_name, self._role_name)
         hdrs = authorization.set_header({'Accept': 'application/json', 'Content-Type': 'application/json'}, self._auth)
         if prod:
-            import sys
-            sys.exit('error: no permissions at the moment')
+            body = {
+              "resourcePermission" : [
+              {
+                "path" : "/",
+                "permissions" : [ "get" ]
+              },
+              {
+                "path" : "/apiproducts",
+                "permissions" : [ "put", "get" ]
+              },
+              {
+                "path" : "/apiproducts/*",
+                "permissions" : [ ]
+              },
+              {
+                "path" : "/apiproducts/" + team_prefix + "*/attributes",
+                "permissions" : [ "put", "get" ]
+              },
+              {
+                "path" : "/apiproducts/" + team_prefix + "*/attributes/*",
+                "permissions" : [ "delete", "put", "get" ]
+              },
+              {
+                "path" : "/apiproducts/" + team_prefix + "*",
+                "permissions" : [ "delete", "put", "get" ]
+              },
+              {
+                "path": "/environments/*/caches/" + team_prefix + "*",
+                "permissions": [ "put", "get", "delete" ]
+              },
+              {
+                "path": "/apiproxies/" + team_prefix + "*/maskconfigs",
+                "permissions": [ "get", "put" ]
+              },
+              {
+                "path" : "/environments/*/keyvaluemaps/" + team_prefix + "*",
+                "permissions" : [ "put", "get" ]
+              },
+              {
+                "path" : "/environments/*/keyvaluemaps/" + team_prefix + "*/entries",
+                "permissions" : [ "put" ]
+              },
+              {
+                "path" : "/developers/*/apps",
+                "permissions" : [ "put", "get" ]
+              },
+              {
+                "path" : "/developers/*/apps*",
+                "permissions" : [  ]
+              },
+              {
+                "path" : "/developers/*/apps/" + team_prefix + "*",
+                "permissions" : [ "delete", "put", "get" ]
+              },
+              {
+                "path" : "/developers/*/apps/" + team_prefix + "*/attributes",
+                "permissions" : [ "put", "get" ]
+              },
+              {
+                "path" : "/developers/*/apps/" + team_prefix + "*/attributes/*",
+                "permissions" : [ "delete", "put", "get" ]
+              },
+              {
+                "path" : "/environments/*/caches",
+                "permissions" : [ "put", "get" ]
+              },
+              {
+                "path" : "/environments/*/caches/*",
+                "permissions" : [ ]
+              },
+              {
+                "path" : "/environments/*/caches/" + team_prefix + "*",
+                "permissions" : [ "delete", "put", "get" ]
+              },
+              {
+                "path" : "/environments/prod/applications/" + team_prefix + "*/revisions/*/debugsessions",
+                "permissions" : [ "put", "get" ]
+              },
+              {
+                "path" : "/environments/prod/applications/" + team_prefix + "*/revisions/*/debugsessions/*",
+                "permissions" : [ "delete", "put", "get" ]
+              },
+              {
+                "path" : "/environments/sandbox/applications/*/revisions/*/debugsessions",
+                "permissions" : [ "put", "get" ]
+              },
+              {
+                "path" : "/environments/sandbox/applications/*/revisions/*/debugsessions/*",
+                "permissions" : [ "delete", "put", "get" ]
+              }
+             ]
+            }
         else:
             body = {
               "resourcePermission" : [
@@ -189,23 +279,47 @@ class Permissions(IPermissions):
                 "permissions" : [ "delete", "put", "get" ]
               },
               {
-                "path" : "/environments/test/applications/*/revisions/*/deployments",
+                "path" : "/environments/prod/applications/" + team_prefix + "*/revisions/*/deployments",
                 "permissions" : [ "delete", "put", "get" ]
               },
               {
-                "path" : "/environments/sandbox/applications/*/revisions/*/deployments",
-                "permissions" : [ "delete", "put", "get" ]
-              },
-              {
-                "path" : "/environments/psm/applications/*/revisions/*/deployments",
-                "permissions" : [ "delete", "put", "get" ]
-              },
-              {
-                "path" : "/environments/*/applications/" + team_prefix + "*/revisions/*/debugsessions",
+                "path" : "/environments/dev/applications/*/revisions/*/debugsessions",
                 "permissions" : [ "put", "get" ]
               },
               {
-                "path" : "/environments/*/applications/" + team_prefix + "*/revisions/*/debugsessions/*",
+                "path" : "/environments/dev/applications/*/revisions/*/debugsessions/*",
+                "permissions" : [ "delete", "put", "get" ]
+              },
+              {
+                "path" : "/environments/sit/applications/*/revisions/*/debugsessions",
+                "permissions" : [ "put", "get" ]
+              },
+              {
+                "path" : "/environments/sit/applications/*/revisions/*/debugsessions/*",
+                "permissions" : [ "delete", "put", "get" ]
+              },
+              {
+                "path" : "/environments/test/applications/*/revisions/*/debugsessions",
+                "permissions" : [ "put", "get" ]
+              },
+              {
+                "path" : "/environments/test/applications/*/revisions/*/debugsessions/*",
+                "permissions" : [ "delete", "put", "get" ]
+              },
+              {
+                "path" : "/environments/sandbox/applications/*/revisions/*/debugsessions",
+                "permissions" : [ "put", "get" ]
+              },
+              {
+                "path" : "/environments/sandbox/applications/*/revisions/*/debugsessions/*",
+                "permissions" : [ "delete", "put", "get" ]
+              },
+              {
+                "path" : "/environments/prod/applications/" + team_prefix + "*/revisions/*/debugsessions",
+                "permissions" : [ "put", "get" ]
+              },
+              {
+                "path" : "/environments/prod/applications/" + team_prefix + "*/revisions/*/debugsessions/*",
                 "permissions" : [ "delete", "put", "get" ]
               }
              ]
