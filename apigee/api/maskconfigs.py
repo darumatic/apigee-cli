bd25347c30869d1558ef2dd101c425a9eaacee71
Matthew Delotavo <matthew.t.delotavo@gmail.com>
2019-12-12 10:23:53 +1100

    Refactor Maskconfigs

--- apigee/api/maskconfigs.py
+++ apigee/api/maskconfigs.py
@@ -14,8 +14,13 @@ class Maskconfigs(IMaskconfigs):
         super().__init__(*args, **kwargs)
 
     def create_data_masks_for_an_api_proxy(self, request_body):
-        uri = '{0}/v1/organizations/{1}/apis/{2}/maskconfigs'.format(APIGEE_ADMIN_API_URL, self._org_name, self._api_name)
-        hdrs = authorization.set_header({'Accept': 'application/json', 'Content-Type': 'application/json'}, self._auth)
+        uri = '{0}/v1/organizations/{1}/apis/{2}/maskconfigs' \
+            .format(APIGEE_ADMIN_API_URL,
+                    self._org_name,
+                    self._api_name)
+        hdrs = authorization.set_header({'Accept': 'application/json',
+                                         'Content-Type': 'application/json'},
+                                        self._auth)
         body = json.loads(request_body)
         resp = requests.post(uri, headers=hdrs, json=body)
         resp.raise_for_status()
@@ -23,32 +28,49 @@ class Maskconfigs(IMaskconfigs):
         return resp
 
     def delete_data_masks_for_an_api_proxy(self, maskconfig_name):
-        uri = '{0}/v1/organizations/{1}/apis/{2}/maskconfigs/{3}'.format(APIGEE_ADMIN_API_URL, self._org_name, self._api_name, maskconfig_name)
-        hdrs = authorization.set_header({'Accept': 'application/json'}, self._auth)
+        uri = '{0}/v1/organizations/{1}/apis/{2}/maskconfigs/{3}' \
+            .format(APIGEE_ADMIN_API_URL,
+                    self._org_name,
+                    self._api_name,
+                    maskconfig_name)
+        hdrs = authorization.set_header({'Accept': 'application/json'},
+                                        self._auth)
         resp = requests.delete(uri, headers=hdrs)
         resp.raise_for_status()
         # print(resp.status_code)
         return resp
 
     def get_data_mask_details_for_an_api_proxy(self, maskconfig_name):
-        uri = '{0}/v1/organizations/{1}/apis/{2}/maskconfigs/{3}'.format(APIGEE_ADMIN_API_URL, self._org_name, self._api_name, maskconfig_name)
-        hdrs = authorization.set_header({'Accept': 'application/json'}, self._auth)
+        uri = '{0}/v1/organizations/{1}/apis/{2}/maskconfigs/{3}' \
+            .format(APIGEE_ADMIN_API_URL,
+                    self._org_name,
+                    self._api_name,
+                    maskconfig_name)
+        hdrs = authorization.set_header({'Accept': 'application/json'},
+                                        self._auth)
         resp = requests.get(uri, headers=hdrs)
         resp.raise_for_status()
         # print(resp.status_code)
         return resp
 
     def list_data_masks_for_an_api_proxy(self):
-        uri = '{0}/v1/organizations/{1}/apis/{2}/maskconfigs'.format(APIGEE_ADMIN_API_URL, self._org_name, self._api_name)
-        hdrs = authorization.set_header({'Accept': 'application/json'}, self._auth)
+        uri = '{0}/v1/organizations/{1}/apis/{2}/maskconfigs' \
+            .format(APIGEE_ADMIN_API_URL,
+                    self._org_name,
+                    self._api_name)
+        hdrs = authorization.set_header({'Accept': 'application/json'},
+                                        self._auth)
         resp = requests.get(uri, headers=hdrs)
         resp.raise_for_status()
         # print(resp.status_code)
         return resp
 
     def list_data_masks_for_an_organization(self):
-        uri = '{0}/v1/organizations/{1}/maskconfigs'.format(APIGEE_ADMIN_API_URL, self._org_name)
-        hdrs = authorization.set_header({'Accept': 'application/json'}, self._auth)
+        uri = '{0}/v1/organizations/{1}/maskconfigs' \
+            .format(APIGEE_ADMIN_API_URL,
+                    self._org_name)
+        hdrs = authorization.set_header({'Accept': 'application/json'},
+                                        self._auth)
         resp = requests.get(uri, headers=hdrs)
         resp.raise_for_status()
         # print(resp.status_code)
