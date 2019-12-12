716812d6b9b2e7de588c97e7878ca7118f1e3c52
Matthew Delotavo <matthew.t.delotavo@gmail.com>
2019-12-12 13:34:57 +1100

    Refactor TargetServer

--- apigee/api/targetservers.py
+++ apigee/api/targetservers.py
@@ -14,8 +14,13 @@ class Targetservers(ITargetservers):
         super().__init__(*args, **kwargs)
 
     def create_a_targetserver(self, environment, request_body):
-        uri = '{0}/v1/organizations/{1}/environments/{2}/targetservers'.format(APIGEE_ADMIN_API_URL, self._org_name, environment)
-        hdrs = authorization.set_header({'Accept': 'application/json', 'Content-Type': 'application/json'}, self._auth)
+        uri = '{0}/v1/organizations/{1}/environments/{2}/targetservers' \
+            .format(APIGEE_ADMIN_API_URL,
+                    self._org_name,
+                    environment)
+        hdrs = authorization.set_header({'Accept': 'application/json',
+                                         'Content-Type': 'application/json'},
+                                        self._auth)
         body = json.loads(request_body)
         resp = requests.post(uri, headers=hdrs, json=body)
         resp.raise_for_status()
@@ -23,32 +28,52 @@ class Targetservers(ITargetservers):
         return resp
 
     def delete_a_targetserver(self, environment):
-        uri = '{0}/v1/organizations/{1}/environments/{2}/targetservers/{3}'.format(APIGEE_ADMIN_API_URL, self._org_name, environment, self._targetserver_name)
-        hdrs = authorization.set_header({'Accept': 'application/json'}, self._auth)
+        uri = '{0}/v1/organizations/{1}/environments/{2}/targetservers/{3}' \
+            .format(APIGEE_ADMIN_API_URL,
+                    self._org_name,
+                    environment,
+                    self._targetserver_name)
+        hdrs = authorization.set_header({'Accept': 'application/json'},
+                                        self._auth)
         resp = requests.delete(uri, headers=hdrs)
         resp.raise_for_status()
         # print(resp.status_code)
         return resp
 
     def list_targetservers_in_an_environment(self, environment, prefix=None):
-        uri = '{0}/v1/organizations/{1}/environments/{2}/targetservers'.format(APIGEE_ADMIN_API_URL, self._org_name, environment)
-        hdrs = authorization.set_header({'Accept': 'application/json'}, self._auth)
+        uri = '{0}/v1/organizations/{1}/environments/{2}/targetservers' \
+            .format(APIGEE_ADMIN_API_URL,
+                    self._org_name,
+                    environment)
+        hdrs = authorization.set_header({'Accept': 'application/json'},
+                                        self._auth)
         resp = requests.get(uri, headers=hdrs)
         resp.raise_for_status()
         # print(resp.status_code)
         return TargetserversSerializer().serialize_details(resp, 'json', prefix=prefix)
 
     def get_targetserver(self, environment):
-        uri = '{0}/v1/organizations/{1}/environments/{2}/targetservers/{3}'.format(APIGEE_ADMIN_API_URL, self._org_name, environment, self._targetserver_name)
-        hdrs = authorization.set_header({'Accept': 'application/json'}, self._auth)
+        uri = '{0}/v1/organizations/{1}/environments/{2}/targetservers/{3}' \
+            .format(APIGEE_ADMIN_API_URL,
+                    self._org_name,
+                    environment,
+                    self._targetserver_name)
+        hdrs = authorization.set_header({'Accept': 'application/json'},
+                                        self._auth)
         resp = requests.get(uri, headers=hdrs)
         resp.raise_for_status()
         # print(resp.status_code)
         return resp
 
     def update_a_targetserver(self, environment, request_body):
-        uri = '{0}/v1/organizations/{1}/environments/{2}/targetservers/{3}'.format(APIGEE_ADMIN_API_URL, self._org_name, environment, self._targetserver_name)
-        hdrs = authorization.set_header({'Accept': 'application/json', 'Content-Type': 'application/json'}, self._auth)
+        uri = '{0}/v1/organizations/{1}/environments/{2}/targetservers/{3}' \
+            .format(APIGEE_ADMIN_API_URL,
+                    self._org_name,
+                    environment,
+                    self._targetserver_name)
+        hdrs = authorization.set_header({'Accept': 'application/json',
+                                         'Content-Type': 'application/json'},
+                                        self._auth)
         body = json.loads(request_body)
         resp = requests.put(uri, headers=hdrs, json=body)
         resp.raise_for_status()
@@ -56,12 +81,10 @@ class Targetservers(ITargetservers):
         return resp
 
     def push_targetserver(self, environment, file):
-        with open(file) as file:
-            body = file.read()
-
+        with open(file) as f:
+            body = f.read()
         targetserver = json.loads(body)
         self._targetserver_name = targetserver['name']
-
         try:
             self.get_targetserver(environment)
             print('Updating', self._targetserver_name)
