e0db306358035eed4d493438c72b7742c34b4727
Matthew Delotavo <matthew.t.delotavo@gmail.com>
2020-02-14 09:14:36 +1100

    Update Sphinx documentation

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Matthew Delotavo'
 author = 'Matthew Delotavo'
 
 # The full version, including alpha/beta/rc tags
-release = '0.23.4'
+release = '0.24.1'
 
 
 # -- General configuration ---------------------------------------------------
