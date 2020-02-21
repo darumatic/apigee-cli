608e487a5af907605834004b4ab11925cb146720
Matthew Delotavo <matthew.t.delotavo@gmail.com>
2020-02-21 19:35:31 +1100

    Update Apiproducts docstrings

--- apigee/api/apiproducts.py
+++ apigee/api/apiproducts.py
@@ -27,11 +27,20 @@ class Apiproducts(IApiproducts):
             auth: Apigee Edge credentials object.
             org_name: Apigee Edge organization.
             apiproduct_name: API Product name.
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
         """
         super().__init__(*args, **kwargs)
 
     def create_api_product(self, request_body):
-        """Create an API product: a list of API resources (URIs) combined with Quota settings that you can use to deliver customized API bundles to your developers."""
+        """Create an API product: a list of API resources (URIs) combined with Quota settings that you can use to deliver customized API bundles to your developers.
+
+        Args:
+            request_body (str): JSON string.
+
+        Returns:
+            requests.Response()
+        """
         uri = f'{APIGEE_ADMIN_API_URL}/v1/organizations/{self._org_name}/apiproducts'
         hdrs = authorization.set_header({'Accept': 'application/json',
                                          'Content-Type': 'application/json'},
@@ -42,7 +51,14 @@ class Apiproducts(IApiproducts):
         return resp
 
     def delete_api_product(self):
-        """Deletes an API Product"""
+        """Deletes an API Product
+
+        Args:
+            None
+
+        Returns:
+            requests.Response()
+        """
         uri = f'{APIGEE_ADMIN_API_URL}/v1/organizations/{self._org_name}/apiproducts/{self._apiproduct_name}'
         hdrs = authorization.set_header({'Accept': 'application/json'},
                                         self._auth)
@@ -51,7 +67,14 @@ class Apiproducts(IApiproducts):
         return resp
 
     def get_api_product(self):
-        """Gets an API Product"""
+        """Gets an API Product
+
+        Args:
+            None
+
+        Returns:
+            requests.Response()
+        """
         uri = f'{APIGEE_ADMIN_API_URL}/v1/organizations/{self._org_name}/apiproducts/{self._apiproduct_name}'
         hdrs = authorization.set_header({'Accept': 'application/json'},
                                         self._auth)
@@ -60,7 +83,23 @@ class Apiproducts(IApiproducts):
         return resp
 
     def list_api_products(self, prefix=None, expand=False, count=1000, startkey=''):
-        """Lists all API Products by name for an organization"""
+        """Lists all API Products by name for an organization
+
+        Args:
+            prefix (str, optional): Filter results by a prefix string. Defaults to None.
+            expand (bool, optional): If True, show product details. Defaults to False.
+            count (int, optional): Apigee Edge for Public Cloud only:
+                Number of API products to return in the API call.
+                The maximum limit is 1000. Use with the startkey to provide more targeted filtering.
+            startkey (str, optional): Apigee Edge for Public Cloud only:
+                Returns a list of API products starting with the specified API product.
+                For example, if you're returning 50 API products at a time (using the count query parameter),
+                you can view products 50-99 by entering the name of the 50th API product in the first API.
+                The API product name is case sensitive.
+
+        Returns:
+            requests.Response()
+        """
         uri = f'{APIGEE_ADMIN_API_URL}/v1/organizations/{self._org_name}/apiproducts?expand={expand}&count={count}&startKey={startkey}'
         hdrs = authorization.set_header({'Accept': 'application/json'},
                                         self._auth)
@@ -69,7 +108,14 @@ class Apiproducts(IApiproducts):
         return ApiproductsSerializer().serialize_details(resp, 'json', prefix=prefix)
 
     def update_api_product(self, request_body):
-        """This method updates an existing API product"""
+        """This method updates an existing API product
+
+        Args:
+            request_body (str): JSON string.
+
+        Returns:
+            requests.Response()
+        """
         uri = f'{APIGEE_ADMIN_API_URL}/v1/organizations/{self._org_name}/apiproducts/{self._apiproduct_name}'
         hdrs = authorization.set_header({'Accept': 'application/json',
                                          'Content-Type': 'application/json'},
@@ -80,9 +126,18 @@ class Apiproducts(IApiproducts):
         return resp
 
     def push_apiproducts(self, file):
-        """Push API product to Apigee
+        """Push API product file to Apigee
 
         This will create an API product if it does not exist and update if it does.
+
+        Args:
+            file (str): The file path.
+
+        Returns:
+            None
+
+        Raises:
+            HTTPError: If response status code is not successful or 404 (GET API Product).
         """
         with open(file) as f:
             body = f.read()
