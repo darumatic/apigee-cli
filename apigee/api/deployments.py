acd3d715c175b62afb9325a0cdd82a00a91778a3
Matthew Delotavo <matthew.t.delotavo@gmail.com>
2020-02-21 21:45:59 +1100

    Refactor Deployments to use f-strings and add docstrings

--- apigee/api/deployments.py
+++ apigee/api/deployments.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python
-"""https://apidocs.apigee.com/api/deployments"""
+"""Source: https://apidocs.apigee.com/api/deployments
+
+API Platform Base Path: https://api.enterprise.apigee.com/v1/o/{org_name}
+
+API Resource Path: /apis/{api_name}/deployments
+API proxies that are actively deployed in environments on Apigee Edge.
+"""
 
 import json
 import requests
@@ -11,18 +17,33 @@ from apigee.util import authorization
 class Deployments(IDeployments):
 
     def __init__(self, *args, **kwargs):
+        """Args:
+            auth: Apigee Edge credentials object.
+            org_name: Apigee Edge organization.
+            api_name: The API Proxy name.
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+        """
         super().__init__(*args, **kwargs)
 
     def get_api_proxy_deployment_details(self, formatted=False, format='text', showindex=False, tablefmt='plain', revision_name_only=False):
-        uri = '{0}/v1/organizations/{1}/apis/{2}/deployments' \
-            .format(APIGEE_ADMIN_API_URL,
-                    self._org_name,
-                    self._api_name)
+        """Gets details for a specific API proxy deployed in a given environment
+
+        Args:
+            formatted (bool, optional): If True, format ``requests.Response()``. Defaults to False.
+            format (str, optional): Specify how to format response. Defaults to 'text'.
+            showindex (bool, optional): If True, show table index. Defaults to False.
+            tablefmt (str, optional): Specify table format. Defaults to 'plain'.
+            revision_name_only (bool, optional): If True, truncate response information. Defaults to False.
+
+        Returns:
+            requests.Response(): Response if ``formatted`` is False, else return a ``formatted`` value.
+        """
+        uri = f'{APIGEE_ADMIN_API_URL}/v1/organizations/{self._org_name}/apis/{self._api_name}/deployments'
         hdrs = authorization.set_header({'Accept': 'application/json'},
                                         self._auth)
         resp = requests.get(uri, headers=hdrs)
         resp.raise_for_status()
-        # print(resp.status_code)
         if formatted:
             if revision_name_only:
                 # return DeploymentsSerializer().serialize_details(resp, 'table', max_colwidth=args.max_colwidth)
