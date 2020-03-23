d955e88579f51fff89916f724205edcc6ffeb024
Matthew Delotavo <matthew.t.delotavo@gmail.com>
2020-03-23 20:04:21 +1100

    Fix AttributeError for module 'builtins'

--- apigee/util/console.py
+++ apigee/util/console.py
@@ -5,17 +5,18 @@ import apigee
 
 
 def log(*message, status=None, silent=False, curr_verbosity=0, expc_verbosity=0):
-    try:
-        hasattr(builtins, "APIGEE_CLI_TOGGLE_SILENT")
-        hasattr(builtins, "APIGEE_CLI_TOGGLE_VERBOSE")
+    toggle_silent = False
+    toggle_verbose = 0
+    try:  # hack
+        toggle_silent = builtins.APIGEE_CLI_TOGGLE_SILENT
+        toggle_verbose = builtins.APIGEE_CLI_TOGGLE_VERBOSE
     except AttributeError:
-        builtins.APIGEE_CLI_TOGGLE_SILENT = False
-        builtins.APIGEE_CLI_TOGGLE_VERBOSE = 0
-    if silent or builtins.APIGEE_CLI_TOGGLE_SILENT:
+        pass
+    if silent or toggle_silent:
         if status:
             sys.exit(status)
         return
-    if curr_verbosity or builtins.APIGEE_CLI_TOGGLE_VERBOSE >= expc_verbosity:
+    if curr_verbosity or toggle_verbose >= expc_verbosity:
         print(*message)
     if status:
         sys.exit(status)
