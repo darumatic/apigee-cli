a213d0a5b5fab307c31690fca61c60b6e370474c
Matthew Delotavo <matthew.t.delotavo@gmail.com>
2020-05-15 23:55:01 +1000

    Clean up package variables

--- apigee/__init__.py
+++ apigee/__init__.py
@@ -10,14 +10,8 @@ import json
 from os import getenv
 from pathlib import Path
 
-APIGEE_CLI_AUTHORIZATION_DEVELOPER_ATTRIBUTE = "team"
 
-APIGEE_CLI_DIRECTORY = str(Path.home() / ".apigee")
-APIGEE_CLI_ACCESS_TOKEN_FILE = str(Path(APIGEE_CLI_DIRECTORY) / "access_token")
-APIGEE_CLI_CREDENTIALS_FILE = str(Path(APIGEE_CLI_DIRECTORY) / "credentials")
-APIGEE_CLI_EXCEPTION_LOG_FILE = str(Path(APIGEE_CLI_DIRECTORY) / "exception.log")
-
-APIGEE_CLI_PREFIX = getenv("APIGEE_CLI_PREFIX")
+# auth
 APIGEE_USERNAME = getenv("APIGEE_USERNAME")
 APIGEE_PASSWORD = getenv("APIGEE_PASSWORD")
 APIGEE_MFA_SECRET = getenv("APIGEE_MFA_SECRET")
@@ -25,5 +19,16 @@ APIGEE_ADMIN_API_URL = "https://api.enterprise.apigee.com"
 APIGEE_OAUTH_URL = "https://login.apigee.com/oauth/token"
 APIGEE_ORG = getenv("APIGEE_ORG")
 
+# custom
+APIGEE_CLI_AUTHORIZATION_DEVELOPER_ATTRIBUTE = "team"
+APIGEE_CLI_PREFIX = getenv("APIGEE_CLI_PREFIX")
+
+# flags
 APIGEE_CLI_TOGGLE_SILENT = False
 APIGEE_CLI_TOGGLE_VERBOSE = 0
+
+# paths
+APIGEE_CLI_DIRECTORY = str(Path.home() / ".apigee")
+APIGEE_CLI_ACCESS_TOKEN_FILE = str(Path(APIGEE_CLI_DIRECTORY) / "access_token")
+APIGEE_CLI_CREDENTIALS_FILE = str(Path(APIGEE_CLI_DIRECTORY) / "credentials")
+APIGEE_CLI_EXCEPTION_LOG_FILE = str(Path(APIGEE_CLI_DIRECTORY) / "exception.log")
