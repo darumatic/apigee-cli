c7bcde8f598ebc2ea1aded7a1a0cf62bad8952df
Matthew Delotavo <matthew.t.delotavo@gmail.com>
2020-07-01 20:51:56 +1000

    Update __version__

--- apigee/__init__.py
+++ apigee/__init__.py
@@ -1,6 +1,6 @@
 APP = 'apigeecli'
 CMD = 'apigee'
-__version__ = '0.42.2'
+__version__ = '0.42.3'
 description = 'Apigee Management API command-line interface with multi-factor authentication'
 long_description = """Apigee Management API command-line interface with multi-factor authentication"""
 
