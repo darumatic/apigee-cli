42c6b79b12b79cb0bd04fd204d5c06217042957e
Matthew Delotavo <matthew.t.delotavo@gmail.com>
2020-08-11 19:56:04 +1000

    Tabulate deployment state

--- apigee/deployments/deployments.py
+++ apigee/deployments/deployments.py
@@ -17,17 +17,21 @@ class DeploymentsSerializer:
         revisions = []
         for i in deployment_details.json()['environment']:
             revisions.append(
-                {'name': i['name'], 'revision': [j['name'] for j in i['revision']]}
+                {
+                    'name': i['name'],
+                    'revision': [j['name'] for j in i['revision']],
+                    'state': [j['state'] for j in i['revision']],
+                }
             )
         if format == 'json':
             return json.dumps(revisions)
         elif format == 'table':
-            table = [[rev['name'], rev['revision']] for rev in revisions]
+            table = [[rev['name'], rev['revision'], rev['state']] for rev in revisions]
             headers = []
             if showindex == 'always' or showindex is True:
-                headers = ['id', 'name', 'revision']
+                headers = ['id', 'name', 'revision', 'state']
             elif showindex == 'never' or showindex is False:
-                headers = ['name', 'revision']
+                headers = ['name', 'revision', 'state']
             return tabulate(table, headers, showindex=showindex, tablefmt=tablefmt)
         else:
             raise ValueError(format)
