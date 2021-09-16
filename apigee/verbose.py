f773f2242f5a077f5f0b3b658932dceb7545c9cd
Matthew Delotavo <matthew@darumatic.com>
2021-09-16 17:46:09 +1000

    :triangular_flag_on_post: Disable logger

--- apigee/verbose.py
+++ apigee/verbose.py
@@ -8,13 +8,12 @@ import requests
 
 def verbose_callback(ctx, param, value):
     builtins.APIGEE_CLI_TOGGLE_VERBOSE = value
-    if value > 0:
-        http_client.HTTPConnection.debuglevel = value - 1
-        logging.basicConfig()
-        logging.getLogger().setLevel(logging.DEBUG)
-        requests_log = logging.getLogger('requests.packages.urllib3')
-        requests_log.setLevel(logging.DEBUG)
-        requests_log.propagate = True
+    http_client.HTTPConnection.debuglevel = value
+    logging.basicConfig()
+    logging.getLogger().setLevel(logging.DEBUG)
+    # requests_log = logging.getLogger('requests.packages.urllib3')
+    # requests_log.setLevel(logging.DEBUG)
+    # requests_log.propagate = True
 
 
 def common_verbose_options(func):
