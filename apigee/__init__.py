2f6aebf05a90d1098d927c16da8abd39b7b41446
mdelotavo <46637675+mdelotavo@users.noreply.github.com>
2023-06-22 19:12:30 +1000

    Update app version

--- apigee/__init__.py
+++ apigee/__init__.py
@@ -6,7 +6,7 @@ from apigee import utils_init
 
 APP = "apigeecli"
 CMD = "apigee"
-__version__ = "0.55.1"
+__version__ = "0.53.0"
 description = "(Unofficial) Apigee Management API command-line interface with multi-factor authentication (MFA) and single sign-on (SSO)/SAML support"
 long_description = """(Unofficial) Apigee Management API command-line interface with multi-factor authentication (MFA) and single sign-on (SSO)/SAML support"""
 
